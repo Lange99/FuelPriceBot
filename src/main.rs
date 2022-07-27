f6f16021ce720acfe16130fecac3c6861ae7507c
Lange99 <c.langellotti@studenti.unibs.it>
2022-07-27 21:11:17 +0200

    changed edition from cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-
-
-
-
-
-
-
-
-
 pub mod request_builder;
 pub mod response;
 pub use utility::get_best_stations;
 pub mod utility;
 
-
-
-
-
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 pub mod telegram_handler;
 pub use telegram_handler::conversation_handler;
 
-
 #[tokio::main]
 async fn main() {
-
     pretty_env_logger::init();
     log::info!("Starting dialogue bot...");
 
